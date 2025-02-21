0e5de5edcc5be6faa2b70ba1d483ad81a4515006
mzlff <veniaminlaktionov@gmail.com>
2025-02-21 20:51:00 +0500

    Update yes.lua

--- yes.lua
+++ yes.lua
@@ -1,6 +1,6 @@
 local animation = Instance.new("Animation", script.Parent)
 animation.AnimationId = "rbxassetid://112382253521044"
-local hum = script.Parent:FindFirstChildOfClass("Humanoid")
+local hum = script.Parent:WaitForChild("Humanoid")
 
 local anim_track = hum:LoadAnimation(animation)
 anim_track:Play(0, 10)
