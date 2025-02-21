c6696dee1338d200dbf04e8407610084c460e9e1
mzlff <veniaminlaktionov@gmail.com>
2025-02-21 20:43:17 +0500

    Add files via upload

---
+++ yes.lua
@@ -0,0 +1,7 @@
+local animation = Instance.new("Animation", script.Parent)
+animation.AnimationId = "rbxassetid://112382253521044"
+local hum = script.Parent:FindFirstChildOfClass("Humanoid")
+
+local anim_track = hum:LoadAnimation(animation)
+anim_track:Play(0, 10)
+anim_track.Looped = true
